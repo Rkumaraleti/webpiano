fb4942000e2520c83b548695ba4a397fec3b4a10
Rkumaraleti <114989409+Rkumaraleti@users.noreply.github.com>
2023-09-10 12:31:08 +0530

    Fixing Audio Preload
    To preload all the audio on page load! This removes the delay while pressing the key in the first instance....

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-let arr = ["z","x","c","q","w","e","r","t","y","u","i","o","p","[","]","v","b","n","m",",",".","s","d","2","3","4","6","7","9","0","-","f","g","j","k","l"];
+let arr = ["z", "x", "c", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "v", "b", "n", "m", ",", ".", "s", "d", "2", "3", "4", "6", "7", "9", "0", "-", "f", "g", "j", "k", "l"];
+
+
 let num = document.querySelectorAll(".key").length;
 
-for(let i = 0; i < num; i++){
+// Preloading the audio
+for (let i = 0; i < num; i++){
+    let sound = "sounds/" + document.querySelectorAll(".key")[i].getAttribute("data-note") + ".mp3";
+    new Audio(sound).load();
+}
+
+// Adding Click Event for each key elem:
+for (let i = 0; i < num; i++){
     document.querySelectorAll(".key")[i].addEventListener("click", function(){
         let sound = "sounds/" + this.getAttribute("data-note") + ".mp3";
         new Audio(sound).play();
-    })
+    })   
 }
 
+// Adding KeyPress Event for each key elem:
 document.addEventListener("keypress", function(e){
     let x = e.key.toLowerCase();
     if(arr.indexOf(x >= 0)) {
